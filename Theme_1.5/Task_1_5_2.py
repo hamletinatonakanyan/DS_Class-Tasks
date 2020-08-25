4a5d3dcf30a5e04ea6471b193992175b7e0b8f06
Hamletina Tonakanyan <hamletinatonakanyan@gmail.com>
2020-08-25 23:51:38 +0400

    OOP Intro: Class: self, method, variable, objects.

---
+++ Theme_1.5/Task_1_5_2.py
@@ -0,0 +1,42 @@
+class Money:
+
+    # defining 2 class variables
+    amount = 0
+    currency = ''
+
+    # as arguments constructor takes amount of currency and it's name
+    def __init__(self, currency_amount, currency_name):
+        self.amount = currency_amount
+        self.currency = currency_name
+
+    # class method for printing currency amount for beginning
+    def print_obj(self):
+        return f'Exists {self.amount} {self.currency}.'
+
+    # class method for summing 2 same type of currencies
+    def sum(self, amount1, amount2):
+        return amount1.amount - amount2.amount
+
+    # class method for subtracting two same type of currencies
+    def sub(self, amount1, amount2):
+        return amount1.amount - amount2.amount
+
+
+# function for defining class objects, making operations with them and printing
+def main():
+
+    baht1 = Money(5000, 'THB')
+    baht2 = Money(1000, 'THB')
+
+    print(baht1.print_obj())
+    print(baht2.print_obj())
+
+    summing = baht1.sum(baht1, baht2)
+    print(f'{baht1.currency} after increasing: {summing}')
+
+    subtract = baht2.sub(baht1, baht2)
+    print(f'{baht1.currency} after decreasing: {subtract}')
+
+
+# calling function
+main()
