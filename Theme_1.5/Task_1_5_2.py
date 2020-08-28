915841e33ccb9138ca9e2a0128bf5080d491c8ad
Hamletina Tonakanyan <hamletinatonakanyan@gmail.com>
2020-08-28 18:37:55 +0400

    OOP Intro: Class: self, method, variable, objects.

--- Theme_1.5/Task_1_5_2.py
+++ Theme_1.5/Task_1_5_2.py
@@ -1,41 +1,41 @@
 class Money:
 
-    # defining 2 class variables
-    amount = 0
-    currency = ''
-
-    # as arguments constructor takes amount of currency and it's name
+    # data members for showing amount of money and currency
     def __init__(self, currency_amount, currency_name):
         self.amount = currency_amount
         self.currency = currency_name
 
-    # class method for printing amount of currency
+    # data method for printing amount of currency
     def print_obj(self):
         print(f'Exists {self.amount} {self.currency}.')
 
-    # class method for summing two currencies
+    # data method for summing two currencies
     def sum(self, other):
-        return self.amount + other.amount
+        return f'After summing: {self.amount + other.amount} {self.currency}'
 
-    # class method for subtracting two currencies
+    # data method for subtracting two currencies
     def sub(self, other):
-        return self.amount - other.amount
+        return f'After subtracting: {self.amount - other.amount} {self.currency}'
 
 
-# function for defining class objects, calling class methods, printing results
+# function for making class objects, calling methods, printing results
 def main():
 
+    # making nwe instances for class Money()
     baht1 = Money(5000, 'THB')
     baht2 = Money(1000, 'THB')
 
+    # printing new instances
     baht1.print_obj()
     baht2.print_obj()
 
+    # summing two currencies of new created class objects and printing result
     summing = baht1.sum(baht2)
-    print(f'{baht1.currency} after increasing: {summing}')
+    print(summing)
 
+    # subtracting two currencies of new created class objects and printing result
     subtract = baht1.sub(baht2)
-    print(f'{baht1.currency} after decreasing: {subtract}')
+    print(subtract)
 
 
 # calling function
