b3942d7f061b3514df1e07704c2951df4f4084ac
Hamletina Tonakanyan <hamletinatonakanyan@gmail.com>
2020-09-09 22:59:25 +0400

    Exceptions

--- Part 1.Python/Theme_1.8/Task_1_8_Rational.py
+++ Part 1.Python/Theme_1.8/Task_1_8_Rational.py
@@ -1,5 +1,6 @@
 class RationalEx:
     def __init__(self, num, denom):
+
         try:
             if type(num) is not int:
                 raise TypeError
@@ -39,151 +40,83 @@ class RationalEx:
     # static method for returning new object with value of least common multiple
     @staticmethod
     def lcm(d1, d2):
-        try:
-            if RationalEx.gcd(d1, d2) == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('static method lcm: denominator can\'t be zero')
-        else:
-            return (d1 * d2) / RationalEx.gcd(d1, d2)
+        return (d1 * d2) / RationalEx.gcd(d1, d2)
 
     # overloading some dunder methods
     def __add__(self, other):
         denom = RationalEx.lcm(self.denominator, other.denominator)
-
-        try:
-            if denom == 0 or denom == 0 or self.denominator == 0 or other.denominator == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __add__: denominator can\'t be zero')
-        else:
-            num = self.numerator * (denom / self.denominator) + other.numerator * (denom / other.denominator)
-            return RationalEx(num, denom)
+        num = self.numerator * (denom / self.denominator) + other.numerator * (denom / other.denominator)
+        return RationalEx(num, denom)
 
     def __sub__(self, other):
         denom = RationalEx.lcm(self.denominator, other.denominator)
-
-        try:
-            if denom == 0 or denom == 0 or self.denominator == 0 or other.denominator == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __sub__: denominator can\'t be zero')
-        else:
-            num = self.numerator * (denom / self.denominator) - other.numerator * (denom / other.denominator)
-            return RationalEx(num, denom)
+        num = self.numerator * (denom / self.denominator) - other.numerator * (denom / other.denominator)
+        return RationalEx(num, denom)
 
     def __mul__(self, other):
         num = self.numerator * other.numerator
         denom = self.denominator * other.denominator
 
-        try:
-            if denom == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __mul__: denominator can\'t be zero')
-        else:
-            return RationalEx(num, denom)
+        return RationalEx(num, denom)
 
     def __truediv__(self, other):
         num = self.numerator * other.denominator
         denom = self.denominator * other.numerator
 
-        try:
-            if denom == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __truediv__: denominator can\'t be zero')
-        else:
-            return RationalEx(num, denom)
+        return RationalEx(num, denom)
 
     def __eq__(self, other):
-        try:
-            if self.denominator == 0 or other.denominator == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __eq__: denominator can\'t be zero')
-        else:
-            if self.numerator == other.numerator and self.denominator == other.denominator:
-                return True
-            return False
+        if self.numerator == other.numerator and self.denominator == other.denominator:
+            return True
+        return False
 
     def __gt__(self, other):
         denom = RationalEx.lcm(self.denominator, other.denominator)
 
-        try:
-            if denom == 0 or self.denominator == 0 or other.denominator == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __gt__: denominator can\'t be zero')
-        else:
-            num1 = denom / self.denominator * self.numerator
-            num2 = denom / other.denominator * other.numerator
-            if num1 > num2:
-                return True
-            return False
+        num1 = denom / self.denominator * self.numerator
+        num2 = denom / other.denominator * other.numerator
+        if num1 > num2:
+            return True
+        return False
 
     def __ge__(self, other):
         denom = RationalEx.lcm(self.denominator, other.denominator)
 
-        try:
-            if denom == 0 or self.denominator == 0 or other.denominator == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __ge__: denominator can\'t be zero')
-        else:
-            num1 = denom / self.denominator * self.numerator
-            num2 = denom / other.denominator * other.numerator
-            if num1 >= num2:
-                return True
-            return False
+        num1 = denom / self.denominator * self.numerator
+        num2 = denom / other.denominator * other.numerator
+        if num1 >= num2:
+            return True
+        return False
 
     def __lt__(self, other):
         denom = RationalEx.lcm(self.denominator, other.denominator)
 
-        try:
-            if denom == 0 or self.denominator == 0 or other.denominator == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __lt__: denominator can\'t be zero')
-        else:
-            num1 = denom / self.denominator * self.numerator
-            num2 = denom / other.denominator * other.numerator
-            if num1 < num2:
-                return True
-            return False
+        num1 = denom / self.denominator * self.numerator
+        num2 = denom / other.denominator * other.numerator
+        if num1 < num2:
+            return True
+        return False
 
     def __le__(self, other):
         denom = RationalEx.lcm(self.denominator, other.denominator)
 
-        try:
-            if denom == 0 or self.denominator == 0 or other.denominator == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __le__: denominator can\'t be zero')
-        else:
-            num1 = denom / self.denominator * self.numerator
-            num2 = denom / other.denominator * other.numerator
-            if num1 <= num2:
-                return True
-            return False
+        num1 = denom / self.denominator * self.numerator
+        num2 = denom / other.denominator * other.numerator
+        if num1 <= num2:
+            return True
+        return False
 
     def __pow__(self, power, modulo=None):
         num = self.numerator ** power
         denom = self.denominator ** power
 
-        try:
-            if denom == 0:
-                raise ZeroDivisionError
-        except ZeroDivisionError:
-            print('RationalE __pow__: denominator can\'t be zero')
-        else:
-            return RationalEx(num, denom)
+        return RationalEx(num, denom)
 
 
 rati1 = RationalEx(7, 6)
-rati2 = RationalEx(5, 0)
+rati2 = RationalEx(5, 2)
 
 try:
-    print(rati1 <= rati2)
+    print(rati1 + rati2)
 except AttributeError:
     print('There is no such attribute')
