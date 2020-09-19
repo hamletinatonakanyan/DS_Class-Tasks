19afb7e1e2cceaf64730201b2601b4dbce9c4ba8
Hamletina Tonakanyan <hamletinatonakanyan@gmail.com>
2020-09-19 20:50:10 +0400

    Booking system

--- Part 1.Python/Project 1.py
+++ Part 1.Python/Project 1.py
@@ -129,17 +129,28 @@ class Hotel:
 
 class Customer:
     """
-    Class: as argument takes list of hotels(class field)
+    Class: as argument takes list of hotels
+    class field:
+    1. hotel_list - list, list of hotels
+    2. room_type - str, the room type reserved by each customer
+    3. room_count_by_type - int, count of room reserved by each customer
+    4. hotel_name - str, name hof hotel where was reserved room by esach customer
     methods:
     1. get_hotel_by_name(): checks if the hotel exists in the list of hotels or not and return Hotel type object
     2. reservation(): as argument takes hotel's name, room's type and count, through calling hotel reservation \
                       returns message about successfully or not room's reserving.
     3. checking_out(): works same as reservation()
     4. rate(): through Hotel.set_rating() rates Hotel, where was reserved the room.
+    5. get_name_of_reserved_hotel(): gets the name of reserved hotel
+    6. get_reserved_room_type(): gets type of reserved room
+    7. get_reserved_room_count_by_type(): gets count of reserved room
     """
 
     def __init__(self, hotel_list):
         self.__hotel_list = hotel_list
+        self.__room_type = None
+        self.__room_count_by_type = 0
+        self.__hotel_name = None
 
     def get_hotel_by_name(self, name):
         for n in self.__hotel_list:
@@ -156,6 +167,9 @@ class Customer:
         except ReservationException:
             print(f"Customer reservation(): There isn't hotel by {hotel_name}.")
         else:
+            self.__hotel_name = hotel_name
+            self.__room_type = room_type
+            self.__room_count_by_type = room_count
             return hotel.reserve(room_type, room_count)
 
     def checking_out(self, hotel_name, room_type, room_count):
@@ -166,8 +180,50 @@ class Customer:
         hotel = self.get_hotel_by_name(hotel_name)
         return hotel.set_rating(rate)
 
+    def get_name_of_reserved_hotel(self):
+        return self.__hotel_name
 
-class Booking:    # to be continued
+    def get_reserved_room_type(self):
+        return self.__room_type
+
+    def get_reserved_room_count_by_type(self):
+        return self.__room_count_by_type
+
+
+class IdGenerator:
+    """
+    Class: as argument gets object's type(class field)
+    method - generate_id(): generates id for specific objects(customer, room, hotel) and returns each one
+    """
+
+    def __init__(self, object_type):
+        self.__type = object_type
+
+    def generate_id(self):
+        import random
+        import string
+        size = 4
+        letters = ''.join(random.sample(string.ascii_letters, size))
+        numbers = random.randint(10, 1000)
+
+        if type(self.__type) is Room:
+            return 'r_' + str(numbers) + letters
+        elif type(self.__type) is Hotel:
+            return 'h_' + str(numbers) + letters
+        elif type(self.__type) is Customer:
+            return 'c_' + str(numbers) + letters
+
+
+class Booking:
+    """
+    Class: argument takes list of customer(class field- type:list)
+    other fields: customer_data - dict(), saves information about each customer's id, reserved hotel, room's type and count
+    methods:
+    1. check_customer(): as argument takes customer, returns- checks if each customer exists in the customer list
+    2. set_customer_data(): as argument takes customer, \
+                            returns information about each customer's id, reserved hotel, room's type and count
+
+    """
 
     def __init__(self, customers_list):
         self.__customers_list = customers_list
@@ -178,57 +234,34 @@ class Booking:    # to be continued
             if customer not in self.__customer_data:
                 raise ValueError
         except ValueError:
-            print(f'Booking.check_customer(): there is not the {customer} customer.')
+            print(f'Booking.check_customer(): there is not the {customer} customer object.')
         else:
             return customer
 
-    @staticmethod
-    def id_generator():
-        import random
-        import string
-        size = 4
-        letters = ''.join(random.sample(string.ascii_letters, size))
-        numbers = random.randint(10, 1000)
-        id = str(numbers) + letters
-        return id
+    def set_customer_data(self, customer):
+        id = IdGenerator(customer).generate_id()
+        hotel_name = customer.get_name_of_reserved_hotel()
+        room_type = customer.get_reserved_room_type()
+        room_count = customer.get_reserved_room_count_by_type()
 
-    def set_customer_data(self):
-        for customer in self.__customers_list:
-            id = self.id_generator()
-            hotel_name = ''                        # ????????????
-            room_type = ''                         # ????????????
-            room_count = ''                        # ????????????
-            self.__customer_data.update(
-                {customer:
-                    {'ID': id,
-                     'hotel_name': hotel_name,
-                     'room_type': room_type,
-                     'room_count': room_count}
-                 }
-            )
-
-    def get_customer_id(self, cst):
-        customer = self.check_customer(cst)
-        customer_id = self.__customer_data[customer]['ID']
-        return customer_id
-
-    def get_customer_chosen_hotel(self, cst):
-        customer = self.check_customer(cst)
-        customer_hotel = self.__customer_data[customer]['hotel_name']
-        return customer_hotel
-
-    def get_customer_chosen_room(self, cst):
-        customer = self.check_customer(cst)
-        customer_room = self.__customer_data[customer]['room_type']
-        return customer_room
-
-    def get_customer_room_count(self, cst):
-        customer = self.check_customer(cst)
-        customer_room_count = self.__customer_data[customer]['room_count']
-        return customer_room_count
+        self.__customer_data.update(
+            {customer:
+                {'ID': id,
+                 'hotel_name': hotel_name,
+                 'room_type': room_type,
+                 'room_count': room_count}
+             }
+        )
+        return self.__customer_data
 
 
 def main():
+    """
+    Function: creates Room, Hotel, Customer types objects, makes list from those objects, \
+              writes information about id, reserved hotel, room, count of rooms \
+              by each customer in csv file named 'room_booking_system.csv'
+    """
+
     room1 = Room('president', 4)
     room2 = Room('double', 10)
     room3 = Room('single', 12)
@@ -248,25 +281,41 @@ def main():
     customer1 = Customer(hotels_list1)
     customer2 = Customer(hotels_list2)
     customer3 = Customer(hotels_list3)
+    customers_list = [customer1, customer2, customer3]
 
     customer1.reservation('Messier87', 'president', 2)
     customer1.checking_out('Messier87', 'president', 2)
     customer1.rate('Messier87', 4.6)
     hotel1.get_rating()
 
-    customers_list = [customer1, customer2, customer3]
-
     booking = Booking(customers_list)
-    booking.set_customer_data()
-    c_id = booking.get_customer_id(customer3)
-    c_hotel = booking.get_customer_chosen_hotel(customer3)
-    room_type = booking.get_customer_chosen_room(customer3)
-    room_count = booking.get_customer_room_count(customer3)
 
-    print("\nCustomer's ID: ", c_id)
-    print("Hotel chosen by customer: ", c_hotel)
-    print("Hotel's room type chosen by customer: ", room_type)
-    print("Hotel's room amount chosen by customer: ", room_count)
+    with open('room_booking_system.csv', 'w') as res:
+        import csv
+        fieldnames = ['customer_id', 'reserved_hotel', 'reserved_room_type', 'reserved_room_count']
+        writer = csv.DictWriter(res, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for customer in customers_list:
+            custom_data = booking.set_customer_data(customer)
+            c_id = custom_data[customer]['ID']
+            c_hotel = custom_data[customer]['hotel_name']
+            room_type = custom_data[customer]['room_type']
+            room_count = custom_data[customer]['room_count']
+
+            if c_hotel is None or room_type is None or room_count is None:
+                c_hotel = 'Null'
+                room_type = 'Null'
+                room_count = 0
+
+            writer.writerow(
+                {'customer_id': c_id,
+                 'reserved_hotel': c_hotel,
+                 'reserved_room_type': room_type,
+                 'reserved_room_count': room_count
+                 }
+            )
 
 
-main()
+if __name__ == "__main__":
+    main()
